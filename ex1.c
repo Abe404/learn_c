c37e0fa8155a84163486de1279a8b5faa75061da
Abe <abe@abesmith.co.uk>
2016-04-28 23:04:38 +0200

    finished ex1

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("good bye world.");
+  puts("its been a fun world.");
+  puts("a mad world.");
+  puts("a bad world.");
+  puts("Hello world.");
+
+  return 0;
+}
