843dd04db92b55a8c187a90b69a40a4ba21d72bf
Abe <abe@abesmith.co.uk>
2016-05-07 07:21:07 +0200

    long int must be cast to double in order to achieve scientific notation

--- src/ex7.c
+++ src/ex7.c
@@ -11,6 +11,7 @@ int main()
   long universe_of_defects = 1L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024;
 
   printf("The entire universe has %ld bugs.\n", universe_of_defects);
+  printf("The entire universe has %.10e bugs.\n", (double)universe_of_defects);
   
   double expected_bugs = bugs * bug_rate;
   printf("You are expected to have %f bugs. \n", expected_bugs);
